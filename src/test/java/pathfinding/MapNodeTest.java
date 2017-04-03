21d89bbee8f32420d62ba4be74779adf131332bb
Ben Longo <benlongo9807@gmail.com>
2017-04-02 23:09:54 -0400

    Create the testing setup.

---
+++ src/test/java/pathfinding/MapNodeTest.java
@@ -0,0 +1,26 @@
+package pathfinding;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by benlo on 4/2/2017.
+ */
+public class MapNodeTest {
+    @Test
+    public void getNeighbors() throws Exception {
+
+    }
+
+    @Test
+    public void heuristicDistanceTo() throws Exception {
+
+    }
+
+    @Test
+    public void distanceTo() throws Exception {
+
+    }
+
+}
