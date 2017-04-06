0776d5431ceb000dd95aa955735815f128889ef8
Ben Longo <benlongo9807@gmail.com>
2017-04-05 22:23:17 -0400

    Start the big backend refactor.

--- src/main/java/service/NodeService.java
+++ src/main/java/service/NodeService.java
@@ -5,6 +5,7 @@ import model.Node;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import java.util.List;
 
 public class NodeService  extends AbstractService<Node> {
     public NodeService(EntityManagerFactory emf) {
@@ -15,5 +16,10 @@ public class NodeService  extends AbstractService<Node> {
     public Node find(Long id) {
         EntityManager manager = this.managerFactory.createEntityManager();
         return manager.find(Node.class, id);
+    }
+
+    public List<Node> neighbors() {
+        EntityManager manager = this.managerFactory.createEntityManager();
+        return manager.createQuery("from Node", Node.class).getResultList();
     }
 }
