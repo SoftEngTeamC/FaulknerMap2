f65c6a4ab68e247354eaa15fb7baf8a429a703a2
Ben Longo <benlongo9807@gmail.com>
2017-04-06 19:37:56 -0400

    Add person loader skeleton and check for blank lines.

--- src/main/java/DataLoader.java
+++ src/main/java/DataLoader.java
@@ -3,13 +3,13 @@ import com.univocity.parsers.tsv.TsvParserSettings;
 import model.Coordinate;
 import model.Node;
 import service.CoordinateService;
+import service.HospitalProfessionalService;
 import service.NodeService;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.xml.crypto.Data;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.List;
 
 public class DataLoader {
@@ -17,7 +17,7 @@ public class DataLoader {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory( "derby" );
         try {
             loadLocations(emf, "data/locations.tsv");
-        } catch (Exception e) {
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         } finally {
             emf.close();
@@ -32,15 +32,33 @@ public class DataLoader {
         // Create all the nodes
         TsvParserSettings parserSettings = new TsvParserSettings();
         TsvParser parser = new TsvParser(parserSettings);
+
         List<String[]> allRows = parser.parseAll(DataLoader.class.getClassLoader().getResourceAsStream(locationsFilePath));
         for (String[] row : allRows.subList(1, allRows.size())) {
-            for (String c : row) System.out.println(c);
+            if (Arrays.asList(row).contains(null)) continue;  // Test for blank line or value
+
+            // Parse the Coordinate
             double x = Double.parseDouble(row[1]);
             double y = Double.parseDouble(row[2]);
             Coordinate location = new Coordinate(x, y, 4);
+
             coordinateService.persist(location);
+
             String name = row[0];
+
             nodeService.persist(new Node(location, name));
         }
+    }
+
+    private static void loadPeople(EntityManagerFactory emf, String peopleFilePath) throws FileNotFoundException {
+        HospitalProfessionalService professionalService = new HospitalProfessionalService(emf);
+
+        TsvParserSettings parserSettings = new TsvParserSettings();
+        TsvParser parser = new TsvParser(parserSettings);
+        List<String[]> allRows = parser.parseAll(DataLoader.class.getClassLoader().getResourceAsStream(peopleFilePath));
+        for (String[] row : allRows.subList(1, allRows.size())) {
+
+        }
+
     }
 }
