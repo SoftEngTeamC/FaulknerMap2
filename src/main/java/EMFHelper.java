615e5dbde00d4939b10864bcae0d156c28a5074d
GGR <ginagonzalezroundey@gmail.com>
2017-04-06 22:25:30 -0400

    Singleton EMF code

---
+++ src/main/java/EMFHelper.java
@@ -0,0 +1,46 @@
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ * Created by Gina on 4/6/17.
+ */
+public class EMFHelper {
+
+    private static EMFHelper myHelper = new EMFHelper();
+    private static EntityManagerFactory emf = null;
+
+
+    //statically check if emf has been created yet
+    //only needs to happen once since singleton
+    static {
+        if (emf == null) {
+            try {
+                emf = Persistence.createEntityManagerFactory("derby");
+                System.out.println("Created new Entity Manager Factory");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Static Accessor Method
+     *
+     * @return
+     */
+    public static EMFHelper getInstance() {
+        if (myHelper == null) {
+            myHelper = new EMFHelper();
+        }
+        return myHelper;
+    }
+
+
+    /**
+     * get Entity Manager Factory
+     * @return
+     */
+    public EntityManagerFactory getEMFactory() {
+        return emf;
+    }
+}
