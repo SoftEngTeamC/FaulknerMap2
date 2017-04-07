6fbba1aaf4e21178c4f64411d063760ea61bc4b1
Ben Longo <benlongo9807@gmail.com>
2017-04-07 14:49:24 -0400

    Merge CustomMapBTests into PathFinderTest.

--- src/test/java/pathfinding/PathFinderTest.java
+++ src/test/java/pathfinding/PathFinderTest.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 public class PathFinderTest {
-    MapNode A, B, C, D, E, F, G, H, I, J, K, L;
+    private MapNode A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V;
 
     @Before
     public void setUp() throws Exception {
@@ -29,6 +29,7 @@ public class PathFinderTest {
         this.A.addNeighbor(C);
         this.B.addNeighbor(C);
         this.C.addNeighbor(D);
+
 
         // The nodes are arranged in a generic star
         // shape as shown bellow where:
@@ -64,6 +65,37 @@ public class PathFinderTest {
         this.G.addNeighbor(I);
         this.H.addNeighbor(J);
         this.H.addNeighbor(L);
+
+
+        //    M --- N ------ V
+        //   /     U  \  <-- MapNode U has no neighbors
+        //  S -- R     \   ... <-Connects to V
+        //  |   /       \ /
+        //  |  Q -- P -- O
+        //  -----------T (MapNode S Connects to MapNode T directly (true path not shown))
+        //MapNodes
+        this.M = new MapNode(new Coordinate(2, 10, 1));
+        this.N = new MapNode(new Coordinate(7, 10, 1));
+        this.O = new MapNode(new Coordinate(10, 2, 1));
+        this.P = new MapNode(new Coordinate(5, 2, 1));
+        this.Q = new MapNode(new Coordinate(2, 2, 1));
+        this.R = new MapNode(new Coordinate(5, 5, 1));
+        this.S = new MapNode(new Coordinate(0, 5, 1));
+        this.T = new MapNode(new Coordinate(8, 0, 1));
+        this.V = new MapNode(new Coordinate(15, 10, 1));
+        //Lone MapNode
+        this.U = new MapNode(new Coordinate(7, 7, 1));
+        //Adding Neighbors
+        this.M.addNeighbor(N);
+        this.N.addNeighbor(O);
+        this.O.addNeighbor(P);
+        this.P.addNeighbor(Q);
+        this.Q.addNeighbor(R);
+        this.R.addNeighbor(S);
+        this.S.addNeighbor(M);
+        this.S.addNeighbor(T);
+        this.N.addNeighbor(V);
+        this.O.addNeighbor(V);
     }
 
     @After
@@ -152,4 +184,54 @@ public class PathFinderTest {
         assertEquals(path, PathFinder.shortestPath(I, I));
     }
 
+    //Checks reverse directionality of addNeighbor()
+    @Test
+    public void testReversePath() throws Exception {
+        List<MapNode> path = new LinkedList<>();
+        path.add(N);
+        path.add(M);
+        assertEquals(PathFinder.shortestPath(N, M), path);
+    }
+
+    //Check node not connected to any other nodes
+    @Test
+    public void testShortestPathIsolated() throws Exception {
+        assertNull(PathFinder.shortestPath(M, U));
+    }
+
+    //Tests to check adjacent nodes choices when determining the path
+    @Test
+    public void testShortJump() throws Exception {
+        List<MapNode> path = new LinkedList<>();
+        path.add(Q);
+        path.add(R);
+        path.add(S);
+        path.add(M);
+        assertEquals(PathFinder.shortestPath(Q, M), path);
+    }
+
+    //Tests to check adjacent nodes choices when determining the path
+    @Test
+    public void testLongJump() throws Exception {
+        List<MapNode> path = new LinkedList<>();
+        path.add(O);
+        path.add(P);
+        path.add(Q);
+        path.add(R);
+        path.add(S);
+        path.add(T);
+        assertEquals(PathFinder.shortestPath(O, T), path);
+    }
+
+    //Tests to check adjacent nodes choices when determining the path
+    @Test
+    public void testHeuristicsPathChoice() throws Exception {
+        List<MapNode> path = new LinkedList<>();
+        path.add(R);
+        path.add(Q);
+        path.add(P);
+        path.add(O);
+        path.add(V);
+        assertEquals(PathFinder.shortestPath(R, V), path);
+    }
 }
