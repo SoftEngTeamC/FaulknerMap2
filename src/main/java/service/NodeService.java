737b6d542b89f7c815f8bc0a468383ade8a7e7ae
Ben Longo <benlongo9807@gmail.com>
2017-04-08 18:28:41 -0400

    Add neighbors method to the NodeService.

--- src/main/java/service/NodeService.java
+++ src/main/java/service/NodeService.java
@@ -1,11 +1,14 @@
 package service;
 
 
+import model.Edge;
 import model.Node;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class NodeService  extends AbstractService<Node> {
     @Override
@@ -14,9 +17,14 @@ public class NodeService  extends AbstractService<Node> {
         return manager.find(Node.class, id);
     }
 
-    private List<Node> neighbors() {
+    private Set<Node> neighbors(Long id) {
         EntityManager manager = this.managerFactory.createEntityManager();
-        return manager.createQuery("from Node", Node.class).getResultList();
+        List<Edge> edges = manager.createQuery("SELECT e FROM Edge e WHERE e.start.id = :id OR e.end.id = :id", Edge.class).setParameter("id", id).getResultList();
+        Set<Node> startNeighbors = edges.stream().map(Edge::getStart).collect(Collectors.toSet());
+        Set<Node> endNeighbors = edges.stream().map(Edge::getStart).collect(Collectors.toSet());
+        startNeighbors.addAll(endNeighbors);
+        startNeighbors.remove(find(id));
+        return startNeighbors;
     }
 
     public Node findNodeByName(String name) {
