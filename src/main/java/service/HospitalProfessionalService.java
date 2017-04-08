f2edb814c20cb83c9f8fdc92bf0b5914df3f7d93
GGR <ginagonzalezroundey@gmail.com>
2017-04-08 17:53:57 -0400

    populated database

--- src/main/java/service/HospitalProfessionalService.java
+++ src/main/java/service/HospitalProfessionalService.java
@@ -4,11 +4,20 @@ package service;
 import model.HospitalProfessional;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 public class HospitalProfessionalService extends AbstractService<HospitalProfessional> {
     @Override
     public HospitalProfessional find(Long id) {
         EntityManager manager = this.managerFactory.createEntityManager();
         return manager.find(HospitalProfessional.class, id);
+    }
+
+    public HospitalProfessional findHospitalProfessionalByName(String name) {
+        EntityManager manager = this.managerFactory.createEntityManager();
+        String sql = "SELECT p FROM HospitalProfessional p where s.name = :name";
+        TypedQuery<HospitalProfessional> query = (TypedQuery<HospitalProfessional>) manager.createQuery(sql);
+        query.setParameter("name", name);
+        return query.getSingleResult();
     }
 }
--- src/main/java/service/HospitalServiceService.java
+++ src/main/java/service/HospitalServiceService.java
@@ -4,11 +4,20 @@ package service;
 import model.HospitalService;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 public class HospitalServiceService extends AbstractService<HospitalService> {
     @Override
     public HospitalService find(Long id) {
         EntityManager manager = this.managerFactory.createEntityManager();
         return manager.find(HospitalService.class, id);
+    }
+
+    public HospitalService findHospitalServiceByName(String name) {
+        EntityManager manager = this.managerFactory.createEntityManager();
+        String sql = "SELECT s FROM HospitalService s where s.name = :name";
+        TypedQuery<HospitalService> query = (TypedQuery<HospitalService>) manager.createQuery(sql);
+        query.setParameter("name", name);
+        return query.getSingleResult();
     }
 }
