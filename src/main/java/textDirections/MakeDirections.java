6180f5f2b1f1bf8bc4b1858af67fd5e2a6120b0c
atags22 <atags22@gmail.com>
2017-04-09 16:09:29 -0400

    Created package for textual directions

---
+++ src/main/java/textDirections/MakeDirections.java
@@ -0,0 +1,12 @@
+package textDirections;
+
+import pathfinding.MapNode;
+
+/**
+ * Created by Alex on 4/9/2017.
+ */
+public class MakeDirections {
+    public static String getText(List<MapNode> myPath){
+        return new String();
+    }
+}
