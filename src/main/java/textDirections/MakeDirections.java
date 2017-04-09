2052e505caf869b6c9cbd3b1023c273f3f90f848
atags22 <atags22@gmail.com>
2017-04-09 16:19:26 -0400

    Added function for getting angle between nodes

--- src/main/java/textDirections/MakeDirections.java
+++ src/main/java/textDirections/MakeDirections.java
@@ -2,11 +2,27 @@ package textDirections;
 
 import pathfinding.MapNode;
 
+import java.util.List;
+
+import static java.lang.Math.atan2;
+
 /**
  * Created by Alex on 4/9/2017.
  */
 public class MakeDirections {
     public static String getText(List<MapNode> myPath){
+
         return new String();
+    }
+
+    /** Angle from starting node A to ending node B
+     * @param a First node
+     * @param b Second node
+     * @return Angle in radians
+     */
+    private double getAngle(MapNode a, MapNode b){
+        double deltaY = b.getLocation().getY() - a.getLocation().getY();
+        double deltaX = b.getLocation().getX() - a.getLocation().getX();
+        return Math.atan2(deltaY, deltaX);
     }
 }
