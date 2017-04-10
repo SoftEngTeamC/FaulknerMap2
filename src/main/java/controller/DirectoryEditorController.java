95b6205718933961e378b656d782c405763fc7ed
pcshingles <pshingletoniv@gmail.com>
2017-04-10 10:53:48 -0400

    Made the edit person and directory editor screens functional, depends on database to work.

--- src/main/java/controller/DirectoryEditorController.java
+++ src/main/java/controller/DirectoryEditorController.java
@@ -2,6 +2,7 @@ package controller;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,13 +10,18 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import model.HospitalProfessional;
+import model.Node;
+import service.HospitalProfessionalService;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
-public class DirectoryEditorController {
+public class DirectoryEditorController extends Controller{
     @FXML
     private Button logoutBtn;
     @FXML
@@ -30,28 +36,41 @@ public class DirectoryEditorController {
     private ListView<String> searchList;
 
     // database helpers
+    HospitalProfessionalService hps;
 
     @FXML
     public void initialize() {
 
+        //init hps
+        hps = new HospitalProfessionalService();
+
+        // disable the edit person button
+        editPrsnBtn.setDisable(true);
+
+        // add a listener to the listview
+        searchList.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            /**
+             * @author paul
+             * enables the edit person button
+             */
+            public void handle(MouseEvent event) {
+                editPrsnBtn.setDisable(false);
+            }});
     }
 
     @FXML
     public void back() throws Exception {
-        Stage stage = (Stage) backBtn.getScene().getWindow();
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("view/AdminToolMenu.fxml"));
-        stage.setTitle("Admin Tool");
-        stage.setScene(new Scene(root, 600, 400));
-        stage.show();
+
+        switchScreen("view/AdminToolMenu.fxml", "Admin tool menu", backBtn);
+
     }
 
     @FXML
     public void logout() throws Exception {
-        Stage stage = (Stage) logoutBtn.getScene().getWindow();
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("view/Main.fxml"));
-        stage.setTitle("Main");
-        stage.setScene(new Scene(root, 600, 400));
-        stage.show();
+
+        switchScreen("view/Main.fxml", "Main screen", logoutBtn);
+
     }
 
     /**
@@ -63,17 +82,21 @@ public class DirectoryEditorController {
      */
     @FXML
     public void editPersonBtnPressed() throws Exception {
-        // get the current hospital professional that is selected in the list
-       // String selectedName = searchList.getSelectionModel().getSelectedItem();
-//        HospitalProfessional selectedPerson = HospitalProfessionalsHelper.getHospitalProfessionalByName(selectedName);
 
-//        // pass it to the next screen
-//        FXMLLoader loader = new FXMLLoader(getClass().getResource("view/EditPersonScreen.fxml"));
-//        Stage stage = new Stage();
-//        stage.setScene(new Scene((Pane)loader.load()));
-//        EditPersonController controller = loader.<EditPersonController>getController();
-////        controller.setSelectedUser(selectedPerson);
-//        stage.show();
+        // Instantiate a database helper
+        HospitalProfessionalService hps = new HospitalProfessionalService();
+
+        // get the current hospital professional that is selected in the list
+        String selectedName = searchList.getSelectionModel().getSelectedItem();
+        HospitalProfessional hp = hps.findHospitalProfessionalByName(selectedName);
+
+        // pass it to the next screen
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("view/EditPersonScreen.fxml"));
+        Stage stage = new Stage();
+        stage.setScene(new Scene((Pane)loader.load()));
+        EditPersonController controller = loader.<EditPersonController>getController();
+        controller.setSelectedUser(hp);
+        stage.show();
 
     }
 
@@ -92,30 +115,33 @@ public class DirectoryEditorController {
         stage.show();
     }
 
-//
-//    /**
-//     * @author Paul
-//     *
-//     * search field entery event handler
-//     * for now, it just populates the list
-//     * <TODO> query the database and populate list with actual results </TODO>
-//     */
-//    @FXML
-    public void searchFieldEntered() {
 
-//        // populate the list with the database
-//        ArrayList<HospitalProfessional> personList = new ArrayList<HospitalProfessional>();
-//        ArrayList<String> nameList = new ArrayList<String>();
-//        personList = HospitalProfessionalsHelper.getHospitalProfessionals(null);
-//        for (HospitalProfessional aPersonList : personList) {
-//            nameList.add(aPersonList.getName());
+    /**
+     * @author Paul
+     *
+     * search field key press event handler
+     * queries database for results, populates list
+     *
+     */
+    @FXML
+    public void searchFieldKeyPressed() {
 
+        // get the query from the searchfield
+        String query = searchField.getText();
+
+        // make and populate list with the names of hospital professionals containing query
+        ArrayList<String> names = new ArrayList<>();
+        List<HospitalProfessional> people = hps.getAllProfessionals();
+        for(HospitalProfessional p : people){
+            if(p.getName().contains(query)){
+                names.add(p.getName());
+            }
         }
-//
-//        // load into the list
-//        ObservableList<String> oList = FXCollections.observableArrayList(nameList);
-//        searchList.setItems(oList);
-//
-//    }
+
+        // set the list to contain the queried strings
+        searchList.setItems(FXCollections.observableList(names));
+
+
+    }
 
 }
--- src/main/java/controller/EditPersonController.java
+++ src/main/java/controller/EditPersonController.java
@@ -1,82 +1,319 @@
 package controller;
 
 
+import javafx.collections.FXCollections;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import model.HospitalProfessional;
+import model.Node;
+import service.HospitalProfessionalService;
+import service.NodeService;
 
-public class EditPersonController {
+import java.util.*;
+
+public class EditPersonController extends Controller{
 
     @FXML
     private Button logoutBtn;
     @FXML
     private Button backBtn;
     @FXML
+    private Button removeNodeBtn;
+    @FXML
+    private Button addNodeBtn;
+    @FXML
+    private Button updateBtn;
+    @FXML
     private TextField nameField;
     @FXML
     private TextField titleField;
     @FXML
-    private TextField locationField;
-    @FXML
     private TextField idField;
     @FXML
-    private Button updateBtn;
-
+    private ListView<String> availableLocationsList;
     @FXML
-    private Text warningText;
+    private ListView<String> locationsList;
+    @FXML
+    private Text updateText;
+    @FXML
+    private Text deletedText;
+
+    // database helpers
+    NodeService ns;
+    HospitalProfessionalService hps;
+
+    // Hospital professional on which we are editing
+    HospitalProfessional hp;
+
+    // Available nodes list
+    List<Node> availableNodeList;
+
+    // current locations list
+    List<Node> currentNodeList;
+
+
+
 
     @FXML
     public void initialize() {
-//        hph = Driver.getHospitalProfessionalHelper();
+
+        // disable update and delete text fields
+        updateText.setVisible(false);
+        deletedText.setVisible(false);
+
+        // initialize database helpers
+        ns = new NodeService();
+        hps = new HospitalProfessionalService();
+
+        // set the name, title, and ID field
+        nameField.setText(this.hp.getName());
+        titleField.setText(this.hp.getTitle());
+        idField.setText(this.hp.getId().toString());
+
+        //remove node button shouldn't be clickable yet
+        removeNodeBtn.setDisable(true);
+
+        // update button shouldn't do anything until something is changed
+        updateBtn.setDisable(true);
+
+        // Add node not enabled yet
+        addNodeBtn.setDisable(true);
+
+        // available nodes list is populated
+        // initialize the private field list
+        availableNodeList = ns.getAllNodes();
+        // make it the set - the person's set of nodes
+        updateAvailableNodesList();
+        // populate the locations listview
+        currentNodeList = hp.getOffice();
+        setListItems(currentNodeList, locationsList);
+
+        // add event handlers for when an item is clicked in both lists
+        locationsList.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            /**
+             * Handler for when someone selects an item. Enables remove node option.
+             */
+            public void handle(MouseEvent event) {
+                removeNodeBtn.setDisable(false);
+            }});
+        availableLocationsList.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            /**
+             * Handler for when the available nodes list is clicked. Enables add node button.
+             */
+            public void handle(MouseEvent event) {
+                addNodeBtn.setDisable(false);
+            }});
+
+
     }
 
+
     @FXML
+    /**
+     * @author paul
+     * goes to admintoolmenu screen
+     */
     public void back() throws Exception {
-        Stage stage = (Stage) backBtn.getScene().getWindow();
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("view/DirectoryEditor.fxml"));
-        stage.setTitle("Directory Editor");
-        stage.setScene(new Scene(root, 600, 400));
-        stage.show();
+
+        switchScreen("view/AdminToolMenu.fxml","Admin tool menu",backBtn);
+
     }
 
     @FXML
+    /**
+     * @author paul
+     * goes to main screen
+     */
     public void logout() throws Exception {
-        Stage stage = (Stage) logoutBtn.getScene().getWindow();
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("view/Main.fxml"));
-        stage.setTitle("Main");
-        stage.setScene(new Scene(root, 600, 400));
-        stage.show();
+
+        switchScreen("view/Main.fxml", "Main menu", logoutBtn);
+
     }
 
-//
-//    public void setSelectedUser(HospitalProfessional hp) {
-//
-//        this.hp = hp;
-//        nameField.setText(this.hp.getName());
-//        titleField.setText(this.hp.getTitle());
-//        locationField.setText(this.hp.getLocation());
-//        idField.setText(this.hp.getId().toString());
-//
-//    }
-//
-//    public void updateBtnPressed() {
-//        // push to database
-//        hp.setName(nameField.getText());
-//        hp.setTitle(titleField.getText());
-//        hp.setLocation(locationField.getText());
-//        hph.updateHospitalProfessional(hp);
-//    }
-//
-//    public void deleteBtnPressed() {
-//        // push to database
-//        hph.deleteHospitalProfessional(hp);
-//        warningText.setVisible(true);
-//    }
+    @FXML
+    /**
+     * @author paul
+     *
+     * Event handler for when the remove node button is clicked.
+     * gets node from database, removes a node, edits database,
+     * refreshes list, disables remove node button.
+     *
+     */
+    public void removeNodeBtnAction(){
+
+        // Get the selected string
+        String selected = locationsList.getSelectionModel().getSelectedItem();
+
+        // remove the selected node from this persons list.
+        removeNode(selected);
+
+        // refresh the lists
+        setListItems(hp.getOffice(), locationsList);
+
+
+        // disable remove node button
+        removeNodeBtn.setDisable(true);
+
+    }
+
+
+
+    @FXML
+    /**
+     * @author paul
+     *
+     * event handler for when the add node butotn is clicked.
+     * gets node from database, edits the person's data in database, refreshes both lists
+     * disables the add node button
+     *
+     */
+    public void addNodeBtnAction(){
+
+        // get referenced node string
+        String selected = availableLocationsList.getSelectionModel().getSelectedItem();
+
+        // edit this person's data in database
+        addNode(selected);
+
+        // refresh the list
+        setListItems(hp.getOffice(), locationsList);
+
+        // refresh available list
+        updateAvailableNodesList();
+
+        // disable add node button
+        addNodeBtn.setDisable(true);
+
+
+    }
+
+    /**
+     * @author paul
+     *
+     * this function is called by the directory editor screen to set the selected professional.
+     *
+     * @param hp
+     */
+    public void setSelectedUser(HospitalProfessional hp) {
+
+        this.hp = hp;
+
+    }
+
+    /**
+     * @author paul
+     *
+     * event handler for the update button being pressed. Updates person in database,
+     * sets the update text to visible
+     */
+    public void updateBtnPressed() {
+        // push to database
+        hp.setName(nameField.getText());
+        hp.setTitle(titleField.getText());
+        hp.setOffice(currentNodeList);
+        // remove old, insert new
+        HospitalProfessional old = hps.find(hp.getId());
+        hps.remove(old);
+        hps.merge(hp);
+        // alert user
+        updateText.setVisible(true);
+
+    }
+
+    /**
+     * @author paul
+     *
+     * deletes person from database, sets deleted text field to visible.
+     *
+     *
+     */
+    public void deleteBtnPressed() {
+        //remove person
+        hps.remove(hp);
+        // indicate to user
+        deletedText.setVisible(true);
+    }
+
+    // ~~~~~~~~~ helpers ~~~~~~~~~
+
+    /**
+     * Updates the local list, then refreshes the available list
+     */
+    private void updateAvailableNodesList() {
+        // remove from the nodeList the elements in the person's office list, if contained
+        for(Node n : hp.getOffice()){
+            if (availableNodeList.contains(n)){ // if contained, remove
+                availableNodeList.remove(n);
+            }
+        }
+        // refresh the list in the view
+        setListItems(availableNodeList, availableLocationsList);
+    }
+
+    /**
+     * @author paul
+     *
+     * Refreshes a given listview given a list of nodes
+     *
+     * @param nodeList list of nodes
+     * @param lv listview to populate
+     */
+    private void setListItems(List<Node> nodeList, ListView lv) {
+        // private list of node names
+        ArrayList<String> names = new ArrayList<>();
+        // loop through nodes, get name and add to list
+        for(Node n : nodeList){
+            names.add(n.getName());
+        }
+        // create FX compatible list and set the LV to this list
+        lv.setItems(FXCollections.observableArrayList(names));
+    }
+
+    /**
+     * @author paul
+     *
+     * removes a node from this person's locations list in database
+     *
+     * @param selected
+     */
+    private void removeNode(String selected) {
+
+        // find the node referenced by string
+        Node n = ns.findNodeByName(selected);
+
+        // edit this list of locations by removing it, then set his offices to updated list
+        currentNodeList.remove(n);
+        hp.setOffice(currentNodeList);
+
+    }
+    /**
+     * @author paul
+     *
+     * adds given node to person's locations list in database
+     *
+     * @param selected
+     *
+     */
+    private void addNode(String selected){
+
+        // find the node referenced by string
+        Node n = ns.findNodeByName(selected);
+
+        // add to this list
+        currentNodeList.add(n);
+        hp.setOffice(currentNodeList);
+    }
 
 }
 
