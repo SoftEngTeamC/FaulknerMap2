3162eca9f8f8f4d3092132ac19e9cd6ac10cd544
Guillermo Rivera <Guillermo@autoreg-717128.wifi.wpi.edu>
2017-04-10 20:30:10 -0400

    more labels

--- src/main/java/controller/HoursEditorController.java
+++ src/main/java/controller/HoursEditorController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -28,6 +29,14 @@ public class HoursEditorController {
     private TextField eveninghrs1;
     @FXML
     private TextField eveninghrs2;
+    @FXML
+    private MenuButton ampm1;
+    @FXML
+    private MenuButton ampm2;
+    @FXML
+    private MenuButton ampm3;
+    @FXML
+    private MenuButton ampm4;
 
     @FXML
     public void back() throws Exception {
