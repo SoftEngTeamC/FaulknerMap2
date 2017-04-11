cb0242bf1d94ca88226015a10fb33f1e8bfae072
GGR <ginagonzalezroundey@gmail.com>
2017-04-11 12:46:32 -0400

    edit node search and tabs

--- src/main/java/service/NodeService.java
+++ src/main/java/service/NodeService.java
@@ -1,6 +1,7 @@
 package service;
 
 
+import model.Coordinate;
 import model.Edge;
 import model.Node;
 
@@ -10,14 +11,14 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public class NodeService  extends AbstractService<Node> {
+public class NodeService extends AbstractService<Node> {
     @Override
     public Node find(Long id) {
         EntityManager manager = this.managerFactory.createEntityManager();
         return manager.find(Node.class, id);
     }
 
-    private Set<Node> neighbors(Long id) {
+    public Set<Node> neighbors(Long id) {
         EntityManager manager = this.managerFactory.createEntityManager();
         List<Edge> edges = manager.createQuery("SELECT e FROM Edge e WHERE e.start.id = :id OR e.end.id = :id", Edge.class)
                 .setParameter("id", id)
@@ -36,7 +37,7 @@ public class NodeService  extends AbstractService<Node> {
                     "SELECT n FROM Node n WHERE n.name LIKE :name", Node.class)
                     .setParameter("name", name)
                     .setMaxResults(1).getSingleResult();
-        } catch (NoResultException e){
+        } catch (NoResultException e) {
             return null;
         }
     }
@@ -53,5 +54,15 @@ public class NodeService  extends AbstractService<Node> {
         EntityManager manager = this.managerFactory.createEntityManager();
         return manager.createQuery("from Node", Node.class)
                 .getResultList();
+    }
+
+    public List<Node> getNodesByFloor(int floor) {
+        EntityManager manager = this.managerFactory.createEntityManager();
+
+        return manager.createQuery("SELECT n FROM Node n," +
+                " Coordinate c WHERE n.location.id = c.id AND " +
+                "c.floor = :floor", Node.class)
+                .setParameter("floor", floor)
+            .getResultList();
     }
 }
