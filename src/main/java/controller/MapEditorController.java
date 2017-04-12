ad1cecff26d43efdc3fa13732646684ac5950ffd
Jon Van Blarcum <jvb2358@gmail.com>
2017-04-12 00:08:17 -0400

    Floor input, Text Indicators of Success, General UI Cleaning, New Maps

--- src/main/java/controller/MapEditorController.java
+++ src/main/java/controller/MapEditorController.java
@@ -1,14 +1,18 @@
 package controller;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.Group;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import model.Coordinate;
 import model.Node;
 import service.CoordinateService;
@@ -18,8 +22,44 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class MapEditorController extends Controller{
+
+    //ImageView Objects
+    @FXML
+    private ScrollPane FirstFloorScrollPane;
+    @FXML
+    private Slider FirstFloorSlider;
+    @FXML
+    private ScrollPane SecondFloorScrollPane;
+    @FXML
+    private Slider SecondFloorSlider;
+    @FXML
+    private ScrollPane ThirdFloorScrollPane;
+    @FXML
+    private Slider ThirdFloorSlider;
+    @FXML
+    private ScrollPane FourthFloorScrollPane;
+    @FXML
+    private Slider FourthFloorSlider;
+    @FXML
+    private ScrollPane FifthFloorScrollPane;
+    @FXML
+    private Slider FifthFloorSlider;
+    @FXML
+    private ScrollPane SixthFloorScrollPane;
+    @FXML
+    private Slider SixthFloorSlider;
+    @FXML
+    private ScrollPane SeventhFloorScrollPane;
+    @FXML
+    private Slider SeventhFloorSlider;
+    //---------------------
+    @FXML
+    private Text MapEditorInstructionText;
+    @FXML
+    private TabPane FloorViewsTabPane;
+    @FXML
+    private VBox MapEditorVBox;
 
     // Back and logout buttons
     @FXML
@@ -36,6 +76,8 @@ public class MapEditorController extends Controller{
     private Button removeNode_searchBtn;
     @FXML
     private Button removeNode_removeBtn;
+    @FXML
+    private Text RemoveNodeIndicatorText;
 
 
     // Add node objects
@@ -45,6 +87,8 @@ public class MapEditorController extends Controller{
     private TextField addNode_xPos;
     @FXML
     private TextField addNode_yPos;
+    @FXML
+    private TextField addNode_floor;
     @FXML
     private ListView<String> addNode_connectedNodesList;
     @FXML
@@ -69,8 +113,8 @@ public class MapEditorController extends Controller{
     private TextField editNode_addField;
     @FXML
     private Button editNode_addBtn;
-
-
+    @FXML
+    private Text AddNodeIndicatorText;
 
     // Map imageview and anchorpane
     @FXML
@@ -90,6 +134,8 @@ public class MapEditorController extends Controller{
 
 
     public void initialize(){
+        InitializeMapViews();
+        InitializeIndicatorTextListeners();
 
         // Set the image view to populate the image
         floor4Image = new Image("file:../Resources/floor4.png");
@@ -107,32 +153,17 @@ public class MapEditorController extends Controller{
             this.nodeList.add(aNode.getName());
         }
 
-        //mouse clicked handler, send x,y data to function
-        anchorPane.setOnMouseClicked(event -> {
-            // get the coordinates
-            double x = event.getX();
-            double y = event.getY();
-            // send to function
-            mouseClicked(x,y);
-        });
+//        //mouse clicked handler, send x,y data to function
+//        anchorPane.setOnMouseClicked(event -> {
+//            // get the coordinates
+//            double x = event.getX();
+//            double y = event.getY();
+//            // send to function
+//            mouseClicked(x,y);
+//        });
 
     }
 
-    /**
-     * Back button action event handler. Opens the Admin page
-     *
-     */
-    public void back() throws IOException {
-        switchScreen("view/AdminToolMenu.fxml", "Directory Editor", backBtn);
-    }
-
-    /**
-     * Action event handler for logout button being pressed. Goes to main screen.
-     *
-     */
-    public void logout() throws IOException {
-        switchScreen("view/Main.fxml", "Main", logoutBtn);
-    }
 
     // Methods for the remove node tab
     /**
@@ -145,7 +176,6 @@ public class MapEditorController extends Controller{
         try {
             String searchField = removeNode_searchField.getText();
             //System.out.println("searchField is: " + searchField);
-
             if(searchField.equals("")){
                 ObservableList<String> allOList = FXCollections.observableArrayList(this.nodeList);
                 removeNode_searchList.setItems(allOList);
@@ -158,7 +188,6 @@ public class MapEditorController extends Controller{
                 //ObservableList<String> OList = FXCollections.observableArrayList(nodeName);
                 //removeNode_searchList.setItems(OList);
             }
-
         }
         catch (Exception E){
             System.out.println("Searching Error");
@@ -183,10 +212,12 @@ public class MapEditorController extends Controller{
         System.out.println(selectNode.getId());
         // print out the node from the database
         try{
-            //TODO make successful text
             this.NS.remove(selectNode);
+            RemoveNodeIndicatorText.setText("Successfully Removed Node");
+            RemoveNodeIndicatorText.setFill(Color.GREEN);
         }catch(Exception e){
-            //TODO make warning text visible
+            RemoveNodeIndicatorText.setText("Unable to Remove Node");
+            RemoveNodeIndicatorText.setFill(Color.RED);
         }
 
         //repopulate the search list
@@ -231,12 +262,20 @@ public class MapEditorController extends Controller{
         CoordinateService CS = new CoordinateService();
         float x = Float.parseFloat(addNode_xPos.getText());
         float y = Float.parseFloat(addNode_yPos.getText());
-        //TODO get floor information
-        //float floor = Float.parseFloat(addNode_floor.getText());
+        float floor = Float.parseFloat(addNode_floor.getText());
         Coordinate addCoord = new Coordinate(x, y, 4);
         CS.persist(addCoord);
         Node newNode = new Node(addCoord, addNode_nameField.getText());
-        NS.merge(newNode);
+        try{
+            //TODO make successful text
+            NS.merge(newNode);
+            AddNodeIndicatorText.setText("Successfully Added Node");
+            AddNodeIndicatorText.setFill(Color.GREEN);
+        }catch(Exception e){
+            //TODO make warning text visible
+            AddNodeIndicatorText.setText("Unable to Add Node");
+            AddNodeIndicatorText.setFill(Color.RED);
+        }
     }
 
 //
@@ -307,8 +346,6 @@ public class MapEditorController extends Controller{
 //        }
 
     }
-
-
     public void imageClicked(){
 
     }
@@ -322,5 +359,169 @@ public class MapEditorController extends Controller{
      */
     private void mouseClicked(double x, double y){
 
+    }
+    //----------------------------------Indicator Text Listeners------------------------------------
+    public void InitializeIndicatorTextListeners(){
+        addNode_xPos.textProperty().addListener(new ChangeListener(){
+            @Override
+            public void changed(ObservableValue arg0, Object arg1, Object arg2){
+                AddNodeIndicatorText.setText("");
+            }
+        });
+        addNode_yPos.textProperty().addListener(new ChangeListener(){
+            @Override
+            public void changed(ObservableValue arg0, Object arg1, Object arg2){
+                AddNodeIndicatorText.setText("");
+            }
+        });
+        addNode_floor.textProperty().addListener(new ChangeListener(){
+            @Override
+            public void changed(ObservableValue arg0, Object arg1, Object arg2){
+                AddNodeIndicatorText.setText("");
+            }
+        });
+        addNode_nameField.textProperty().addListener(new ChangeListener(){
+            @Override
+            public void changed(ObservableValue arg0, Object arg1, Object arg2){
+                AddNodeIndicatorText.setText("");
+            }
+        });
+        removeNode_searchField.textProperty().addListener(new ChangeListener(){
+            @Override
+            public void changed(ObservableValue arg0, Object arg1, Object arg2){
+                AddNodeIndicatorText.setText("");
+            }
+        });
+    }
+
+    //----------------------------------Sreen Changing Functions-------------------------------------
+    /**
+     * Back button action event handler. Opens the Admin page
+     */
+    public void back() throws IOException {
+        switchScreen("view/AdminToolMenu.fxml", "Directory Editor", backBtn);
+    }
+
+    /**
+     * Action event handler for logout button being pressed. Goes to main screen.
+     */
+    public void logout() throws IOException {
+        switchScreen("view/Main.fxml", "Main", logoutBtn);
+    }
+
+    //----------------------------------Build Zoomable Maps----------------------------------------------
+    public void InitializeMapViews(){
+        //FIRST FLOOR
+        FirstFloorScrollPane.prefWidthProperty().bind(FloorViewsTabPane.widthProperty());
+        FirstFloorScrollPane.prefHeightProperty().bind(FloorViewsTabPane.heightProperty());
+        ImageView FirstFloorImageView = new ImageView();
+        Image FirstFloorMapPic = new Image("images/1_thefirstfloor.png");
+        FirstFloorImageView.setImage(FirstFloorMapPic);
+        FirstFloorImageView.setPreserveRatio(true);
+        Group FirstFloorGroup = new Group();
+        FirstFloorGroup.getChildren().add(FirstFloorImageView);
+        FirstFloorScrollPane.setContent(FirstFloorGroup);
+        FirstFloorScrollPane.setPannable(true);
+        FirstFloorScrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        FirstFloorScrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        FirstFloorSlider.setMax(FirstFloorMapPic.getWidth());
+        FirstFloorSlider.minProperty().bind(FloorViewsTabPane.widthProperty());
+        FirstFloorImageView.fitWidthProperty().bind(FirstFloorSlider.valueProperty());
+        //SECOND FLOOR
+        SecondFloorScrollPane.prefWidthProperty().bind(FloorViewsTabPane.widthProperty());
+        SecondFloorScrollPane.prefHeightProperty().bind(FloorViewsTabPane.heightProperty());
+        ImageView SecondFloorImageView = new ImageView();
+        Image SecondFloorMapPic = new Image("images/2_thesecondfloor.png");
+        SecondFloorImageView.setImage(FirstFloorMapPic);
+        SecondFloorImageView.setPreserveRatio(true);
+        Group SecondFloorGroup = new Group();
+        SecondFloorGroup.getChildren().add(SecondFloorImageView);
+        SecondFloorScrollPane.setContent(SecondFloorGroup);
+        SecondFloorScrollPane.setPannable(true);
+        SecondFloorScrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        SecondFloorScrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        SecondFloorSlider.setMax(SecondFloorMapPic.getWidth());
+        SecondFloorSlider.minProperty().bind(FloorViewsTabPane.widthProperty());
+        SecondFloorImageView.fitWidthProperty().bind(SecondFloorSlider.valueProperty());
+        //THIRD FLOOR
+        ThirdFloorScrollPane.prefWidthProperty().bind(FloorViewsTabPane.widthProperty());
+        ThirdFloorScrollPane.prefHeightProperty().bind(FloorViewsTabPane.heightProperty());
+        ImageView ThirdFloorImageView = new ImageView();
+        Image ThirdFloorMapPic = new Image("images/3_thethirdfloor.png");
+        ThirdFloorImageView.setImage(ThirdFloorMapPic);
+        ThirdFloorImageView.setPreserveRatio(true);
+        Group ThirdFloorGroup = new Group();
+        ThirdFloorGroup.getChildren().add(ThirdFloorImageView);
+        ThirdFloorScrollPane.setContent(ThirdFloorGroup);
+        ThirdFloorScrollPane.setPannable(true);
+        ThirdFloorScrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        ThirdFloorScrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        ThirdFloorSlider.setMax(ThirdFloorMapPic.getWidth());
+        ThirdFloorSlider.minProperty().bind(FloorViewsTabPane.widthProperty());
+        ThirdFloorImageView.fitWidthProperty().bind(ThirdFloorSlider.valueProperty());
+        //FOURTH FLOOR
+        FourthFloorScrollPane.prefWidthProperty().bind(FloorViewsTabPane.widthProperty());
+        FourthFloorScrollPane.prefHeightProperty().bind(FloorViewsTabPane.heightProperty());
+        ImageView FourthFloorImageView = new ImageView();
+        Image FourthFloorMapPic = new Image("images/4_thefourthfloor.png");
+        FourthFloorImageView.setImage(FourthFloorMapPic);
+        FourthFloorImageView.setPreserveRatio(true);
+        Group FourthFloorGroup = new Group();
+        FourthFloorGroup.getChildren().add(FourthFloorImageView);
+        FourthFloorScrollPane.setContent(FourthFloorGroup);
+        FourthFloorScrollPane.setPannable(true);
+        FourthFloorScrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        FourthFloorScrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        FourthFloorSlider.setMax(FourthFloorMapPic.getWidth());
+        FourthFloorSlider.minProperty().bind(FloorViewsTabPane.widthProperty());
+        FourthFloorImageView.fitWidthProperty().bind(FourthFloorSlider.valueProperty());
+        //FIFTH FLOOR
+        FifthFloorScrollPane.prefWidthProperty().bind(FloorViewsTabPane.widthProperty());
+        FifthFloorScrollPane.prefHeightProperty().bind(FloorViewsTabPane.heightProperty());
+        ImageView FifthFloorImageView = new ImageView();
+        Image FifthFloorMapPic = new Image("images/5_thefifthfloor.png");
+        FifthFloorImageView.setImage(FifthFloorMapPic);
+        FifthFloorImageView.setPreserveRatio(true);
+        Group FifthFloorGroup = new Group();
+        FifthFloorGroup.getChildren().add(FifthFloorImageView);
+        FifthFloorScrollPane.setContent(FifthFloorGroup);
+        FifthFloorScrollPane.setPannable(true);
+        FifthFloorScrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        FifthFloorScrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        FifthFloorSlider.setMax(FifthFloorMapPic.getWidth());
+        FifthFloorSlider.minProperty().bind(FloorViewsTabPane.widthProperty());
+        FifthFloorImageView.fitWidthProperty().bind(FifthFloorSlider.valueProperty());
+        //SIXTH FLOOR
+        SixthFloorScrollPane.prefWidthProperty().bind(FloorViewsTabPane.widthProperty());
+        SixthFloorScrollPane.prefHeightProperty().bind(FloorViewsTabPane.heightProperty());
+        ImageView SixthFloorImageView = new ImageView();
+        Image SixthFloorMapPic = new Image("images/6_thesixthfloor.png");
+        SixthFloorImageView.setImage(SixthFloorMapPic);
+        SixthFloorImageView.setPreserveRatio(true);
+        Group SixthFloorGroup = new Group();
+        SixthFloorGroup.getChildren().add(SixthFloorImageView);
+        SixthFloorScrollPane.setContent(SixthFloorGroup);
+        SixthFloorScrollPane.setPannable(true);
+        SixthFloorScrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        SixthFloorScrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        SixthFloorSlider.setMax(SixthFloorMapPic.getWidth());
+        SixthFloorSlider.minProperty().bind(FloorViewsTabPane.widthProperty());
+        SixthFloorImageView.fitWidthProperty().bind(SixthFloorSlider.valueProperty());
+        //SEVENTH FLOOR
+        SeventhFloorScrollPane.prefWidthProperty().bind(FloorViewsTabPane.widthProperty());
+        SeventhFloorScrollPane.prefHeightProperty().bind(FloorViewsTabPane.heightProperty());
+        ImageView SeventhFloorImageView = new ImageView();
+        Image SeventhFloorMapPic = new Image("images/7_theseventhfloor.png");
+        SeventhFloorImageView.setImage(SeventhFloorMapPic);
+        SeventhFloorImageView.setPreserveRatio(true);
+        Group SeventhFloorGroup = new Group();
+        SeventhFloorGroup.getChildren().add(SeventhFloorImageView);
+        SeventhFloorScrollPane.setContent(SeventhFloorGroup);
+        SeventhFloorScrollPane.setPannable(true);
+        SeventhFloorScrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        SeventhFloorScrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        SeventhFloorSlider.setMax(SeventhFloorMapPic.getWidth());
+        SeventhFloorSlider.minProperty().bind(FloorViewsTabPane.widthProperty());
+        SeventhFloorImageView.fitWidthProperty().bind(SeventhFloorSlider.valueProperty());
     }
 }
