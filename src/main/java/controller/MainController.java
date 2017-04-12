55ca125c328388c6087184a414071e7743d62433
wangyaofeng <yaofengwang729@hotmail.com>
2017-04-11 23:31:05 -0400

    start, dest, and switch

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -5,8 +5,6 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-
-import javafx.geometry.Orientation;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -15,7 +13,6 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
@@ -27,8 +24,6 @@ import pathfinding.PathFinder;
 import service.HospitalProfessionalService;
 import service.NodeService;
 
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 public class MainController extends Controller{
@@ -83,6 +78,19 @@ public class MainController extends Controller{
     private VBox MainVbox;
     @FXML
     private HBox CheckBoxesHBox;
+    @FXML
+    private TextArea Start_location_TextArea;
+    @FXML
+    private TextArea Dest_location_TextArea;
+    @FXML
+    private HBox PathLocationHBox;
+    @FXML
+    private Button SetStartLocationButton;
+    @FXML
+    private Button SetDestLocationButton;
+    @FXML
+    private Button getPathButton;
+
 
     private static int language; // 1: english, 2: spanish, 3: chinese, 4: french
 
@@ -95,6 +103,8 @@ public class MainController extends Controller{
         DisplayInformationTextArea.prefHeightProperty().bind(MainVbox.heightProperty().multiply(0.5));
         DisplayInformationTextArea.prefHeightProperty().bind(MainVbox.heightProperty().multiply(0.5));
         CheckBoxesHBox.setPrefHeight(30);
+
+        PathLocationHBox.prefHeightProperty().bind(MainVbox.heightProperty().multiply(0.05));
 
 
 
@@ -246,11 +256,11 @@ public class MainController extends Controller{
         SearchResultsListView.setItems(data);
     }
 
-    public void FindandDisplayPath(HospitalProfessional HP) {
+    public void FindandDisplayPath(HospitalProfessional HP_Start, HospitalProfessional HP_Dest) {
         NodeService NS = new NodeService();
         pathfinding.Map map = new pathfinding.Map(NS.getAllNodes());
-        MapNode start = map.getNode(NS.findNodeByName("Radiology").getId());
-        MapNode dest = map.getNode(HP.getId());
+        MapNode start = map.getNode(HP_Start.getId());
+        MapNode dest = map.getNode(HP_Dest.getId());
         List<MapNode> path = PathFinder.shortestPath(start, dest);
         DisplayMap(path);
     }
@@ -294,8 +304,59 @@ public class MainController extends Controller{
         System.out.println("clicked on " + SearchResultsListView.getSelectionModel().getSelectedItem());
         HospitalProfessionalService HPS = new HospitalProfessionalService();
         PopulateInformationDisplay(HPS.findHospitalProfessionalByName(SearchResultsListView.getSelectionModel().getSelectedItem().toString()));
-        FindandDisplayPath(HPS.findHospitalProfessionalByName(SearchResultsListView.getSelectionModel().getSelectedItem().toString()));
     }
+
+
+    // function after clicking set start location
+    public void SetStartLocationButtonClicked(){
+        System.out.println("clicked on Set Start button");
+        HospitalProfessionalService HPS = new HospitalProfessionalService();
+        HPS.findHospitalProfessionalByName(SearchResultsListView.getSelectionModel().getSelectedItem().toString());
+        HospitalProfessional HP = new HospitalProfessional();
+        HP = HPS.findHospitalProfessionalByName(SearchResultsListView.getSelectionModel().getSelectedItem().toString());
+        Start_location_TextArea.setText(HP.getName());
+    }
+
+    // function after clicking set destination location
+    public void SetDestLocationButtonClicked(){
+        System.out.println("clicked on Set Dest button");
+        HospitalProfessionalService HPS = new HospitalProfessionalService();
+        HPS.findHospitalProfessionalByName(SearchResultsListView.getSelectionModel().getSelectedItem().toString());
+        HospitalProfessional HP = new HospitalProfessional();
+        HP = HPS.findHospitalProfessionalByName(SearchResultsListView.getSelectionModel().getSelectedItem().toString());
+        Dest_location_TextArea.setText(HP.getName());
+    }
+
+    public void switchLocationButtonClicked(){
+        System.out.println("clicked on switch location button");
+
+
+        String tempStorage = Start_location_TextArea.getText();
+        System.out.println("tempStorage is" + tempStorage);
+        Start_location_TextArea.setText(Dest_location_TextArea.getText());
+        Dest_location_TextArea.setText(tempStorage);
+
+        HospitalProfessionalService switched_Dest = new HospitalProfessionalService();
+        switched_Dest.findHospitalProfessionalByName(Start_location_TextArea.getText());
+
+    }
+
+
+    public void getPathButtonClicked(){
+        System.out.println("clicked on get path button");
+        HospitalProfessionalService HPS_Start = new HospitalProfessionalService();
+        HospitalProfessional HP_Start = HPS_Start.findHospitalProfessionalByName(Start_location_TextArea.getText());
+
+        HospitalProfessionalService HPS_Dest = new HospitalProfessionalService();
+        HospitalProfessional HP_Dest = HPS_Dest.findHospitalProfessionalByName(Dest_location_TextArea.getText());
+
+        FindandDisplayPath(HP_Start,HP_Dest);
+
+        System.out.println("start HP:  " + HP_Start.getName());
+        System.out.println("dest HP:  " + HP_Dest.getName());
+    }
+
+
 
     public void SearchBarTextField_keyReleased() {
         System.out.println("Searching");
