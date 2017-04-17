10326624a5a139cc7bd11f8b3106f5db828f8a7e
Jon Van Blarcum <jvb2358@gmail.com>
2017-04-17 15:18:38 -0400

    keeps nodes selected across tabs in MapEditor

--- src/main/java/controller/MapEditorController.java
+++ src/main/java/controller/MapEditorController.java
@@ -179,6 +179,7 @@ public class MapEditorController extends Controller {
 
         ShowNodesEdgesHelper.InitializeMapViews();
         InitializeIndicatorTextListeners();
+        removeNode_searchFieldValueListner();
 
         // init local lists
         searchList = new ArrayList<>();
@@ -200,12 +201,13 @@ public class MapEditorController extends Controller {
         ObservableList<String> obList = FXCollections.observableArrayList(nameList);
 
         editNode_searchResultsList.setItems(obList);
+        removeNode_searchList.setItems(obList);
         disableEdge_searchResultsList.setItems(obList);
 
 
         tabPaneListen();
         System.out.println("INITRemoveNaighboreListener:");
-        removeNeighborListen();
+        setEditNode_searchResultsListening();
         System.out.println("INITShowNodes:");
         List<Circle> circles = ShowNodesEdgesHelper.showNodes(currFloor);
         System.out.println("INITcirclesListen:");
@@ -213,6 +215,8 @@ public class MapEditorController extends Controller {
         //List<Edge> Edges = ShowNodesEdgesHelper.getEdges(currFloor);
     }
 
+
+    //-------------------------------------------Listeners---------------------------------------------
     public void circlesListen(List<Circle> circles, int floor){
         System.out.println("circlesListen:");
         final Circle[] firstCircle = new Circle[1];
@@ -262,13 +266,7 @@ public class MapEditorController extends Controller {
         }
     }
 
-    //This function listens for a click on anything that isnt a circle.
-    //Then it removes all the highlights and clears the firstCircle[0]
-    public void ClickOnMapListener(int floor){
-
-
-    }
-
+    //This Listener is trggered when a different MapTab is selected
 
     public void tabPaneListen() {
         System.out.println("TabPaneListener");
@@ -304,17 +302,22 @@ public class MapEditorController extends Controller {
                     }
                 }
         );
-
-
     }
 
-    public void removeNeighborListen() {
+    //This Listener is triggered when an item in the EditNode_SearchResults List is selected
+    public void setEditNode_searchResultsListening() {
         editNode_searchResultsList.getSelectionModel().selectedItemProperty()
                 .addListener((observable, oldValue, newValue) -> {
                     System.out.println("editNode_SearchResultsList Listener");
                     //Update Current Node
                     Node selectedNode = NS.findNodeByName(newValue);
                     currNodes[0] = selectedNode;
+                    ObservableList<String> RemoveNodeList = removeNode_searchList.getItems();
+                    int j=0;
+                    for(j=0; !newValue.equals(removeNode_searchList.getItems().get(j));j++){
+                        //find the index of the item in the list
+                    }
+                    removeNode_searchList.getSelectionModel().select(j);
                     //Update Neighbors List
                     ObservableList<String> nList = FXCollections.observableArrayList(neighborNames(currNodes[0]));
                     editNode_neighborsList.setItems(nList);
@@ -340,6 +343,7 @@ public class MapEditorController extends Controller {
                     }
                 });
 
+        //This listener is triggered when the EditNode_Neighbors List is selected
         editNode_neighborsList.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
             System.out.println("EditNode_NeighborsList Listener");
             //clear Old Highlights
@@ -381,8 +385,6 @@ public class MapEditorController extends Controller {
 
     }
 
-
-    // Methods for the remove node tab
 
     /**
      * @author Samuel Coache
@@ -445,19 +447,22 @@ public class MapEditorController extends Controller {
     /**
      * method that populates the search results with the search query
      */
-    public void removeNode_searchFieldKeyPressed() {
-        // get the query from the field
-        String query = removeNode_searchField.getText();
-        ArrayList<String> queryList = new ArrayList<>();
-        // add each query to the list
-        List<Node> nodeList = this.NS.getAllNodes();
-        for (Node n : nodeList) {
-            if (n.getName().contains(query))
-                queryList.add(n.getName());
-        }
-        // Make the list view show the results
-        Collections.sort(queryList, String.CASE_INSENSITIVE_ORDER);
-        removeNode_searchList.setItems(FXCollections.observableArrayList(queryList));
+    public void removeNode_searchFieldValueListner() {
+        removeNode_searchField.textProperty().addListener((observable, oldValue, newValue) -> {
+            // get the query from the field
+            String query = newValue;
+            ArrayList<String> queryList = new ArrayList<>();
+            // add each query to the list
+            List<Node> nodeList = this.NS.getNodesByFloor(currFloor);
+            for (Node n : nodeList) {
+                if (n.getName().contains(query)) {
+                    queryList.add(n.getName());
+                }
+            }
+            // Make the list view show the results
+            Collections.sort(queryList, String.CASE_INSENSITIVE_ORDER);
+            removeNode_searchList.setItems(FXCollections.observableArrayList(queryList));
+        });
     }
 
     /**
