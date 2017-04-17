7bdd01563288633e17361366cd65b72a236a48ad
wangyaofeng <yaofengwang729@hotmail.com>
2017-04-17 19:57:29 -0400

    merge mac page bug

--- src/main/java/controller/AboutUsController.java
+++ src/main/java/controller/AboutUsController.java
@@ -1,14 +1,48 @@
 package controller;
 
-
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextBoundsType;
 import javafx.stage.Stage;
+import model.Edge;
+import model.HospitalProfessional;
+
+import model.Node;
+import pathfinding.MapNode;
+import pathfinding.PathFinder;
+import service.EdgeService;
+
+import pathfinding.Map;
+import pathfinding.MapNode;
+import pathfinding.PathFinder;
+import service.EMFProvider;
+import service.HospitalProfessionalService;
+import service.NodeService;
+import model.Hours;
+import textDirections.MakeDirections;
+
+import java.awt.event.MouseEvent;
+import java.util.Collections;
+
+import java.util.LinkedList;
+import java.util.List;
 
 
 public class AboutUsController extends Controller{
@@ -22,7 +56,7 @@ public class AboutUsController extends Controller{
     @FXML
     public void OpenMeetTheGroup() throws Exception {
         // goto genres screen
-        System.out.println("HERE WE ARE");
+        System.out.println("Meet the group");
         Stage stage = (Stage) MeetTheTeamButton.getScene().getWindow();
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("view/MeetTheGroup.fxml"));
         stage.setTitle("Meet The Group");
--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -2,7 +2,6 @@ package controller;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
@@ -13,33 +12,20 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Paint;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Line;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextBoundsType;
 import javafx.stage.Stage;
 import model.Edge;
 import model.HospitalProfessional;
-
+import model.Hours;
 import model.Node;
-import pathfinding.MapNode;
-import pathfinding.PathFinder;
-import service.EdgeService;
-
 import pathfinding.Map;
 import pathfinding.MapNode;
 import pathfinding.PathFinder;
 import service.EMFProvider;
+import service.EdgeService;
 import service.HospitalProfessionalService;
 import service.NodeService;
-import model.Hours;
 import textDirections.MakeDirections;
-
-import java.awt.event.MouseEvent;
-import java.util.Collections;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -616,7 +602,7 @@ public class MainController extends Controller {
     @FXML
     public void OpenAboutUs() throws Exception {
         // goto genres screen
-        System.out.println("HERE WE ARE");
+        System.out.println("about us");
         Stage stage = (Stage) AboutUsButton.getScene().getWindow();
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("view/AboutUs.fxml"));
         stage.setTitle("AboutUs");
