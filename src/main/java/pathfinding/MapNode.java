ac4d6ef0306615405a8344b0916f85b27e23fda5
atags22 <atags22@gmail.com>
2017-04-18 19:41:29 -0400

    Fix OB1 error. Less than signs are confusing sometimes!

--- src/main/java/pathfinding/MapNode.java
+++ src/main/java/pathfinding/MapNode.java
@@ -30,7 +30,7 @@ public class MapNode implements Node<MapNode> {
 
     public static double getPixels(List<MapNode> myPath) {
         int totalDistance = 0;
-        for(int i = 0; i < myPath.size()-2;i++){ //Go until 1 before the end
+        for(int i = 0; i < myPath.size()-1;i++){ //Go until 1 before the end
             totalDistance += myPath.get(i).distanceTo(myPath.get(i+1));
         }
         return totalDistance;
