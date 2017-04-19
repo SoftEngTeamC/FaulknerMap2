a6cd5f886f8f093cae724e5a0b1654287626ade8
Ben Longo <benlongo9807@gmail.com>
2017-04-19 03:36:53 -0400

    Stop doing shit to stairs.

--- src/main/java/DataLoader.java
+++ src/main/java/DataLoader.java
@@ -60,7 +60,6 @@ public class DataLoader {
 
             loadEdges("data/allEdges.tsv", 1);
             connectElevators();
-            connectStairs();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } finally {
@@ -255,18 +254,6 @@ public class DataLoader {
                     edgeService.persist(new Edge(n1, n2, 0));
                 }
             }
-        }
-    }
-
-    private static void connectStairs() {
-        NodeService nodeService = new NodeService();
-        EdgeService edgeService = new EdgeService();
-
-        List<Node> stairs = nodeService.getAllNodes().stream()
-                .filter(n -> n.getName().toLowerCase().contains("stair"))
-                .collect(Collectors.toList());
-        for (Node s1 : stairs) {
-            for (Node s2 : stairs) edgeService.persist(new Edge(s1, s2, 0));
         }
     }
 
