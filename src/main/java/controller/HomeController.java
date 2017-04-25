dcf01b7e949b575a2d88ae2cc9ebaec48fc3e52d
Jon Van Blarcum <jvb2358@gmail.com>
2017-04-25 16:36:15 -0400

    InitializeMap

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -8,9 +8,15 @@ import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Slider;
 import javafx.scene.control.SplitPane;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
+import pathfinding.MapNode;
+import pathfinding.Node;
+import pathfinding.Path;
 
 public class HomeController {
 
@@ -42,6 +48,9 @@ public class HomeController {
     private SplitPane Map_Split;
 
     @FXML
+    private AnchorPane Map_AnchorPane;
+
+    @FXML
     private ScrollPane Map_ScrollPane;
 
     @FXML
@@ -53,12 +62,52 @@ public class HomeController {
     @FXML
     private HBox Map_HBox;
 
+    ImageView MapImageView = new ImageView();
     @FXML
     void initialize() {
-        ImageView Map = new ImageView();
-//        Image MapPic =
-
-        Group MapGroup = new Group();
-
+        InitializeMap();
     }
+    private void InitializeMap(){
+        Map_ScrollPane.prefWidthProperty().bind(Map_AnchorPane.widthProperty());
+        Map_ScrollPane.prefHeightProperty().bind(Map_AnchorPane.heightProperty());
+        MapImageView.setPreserveRatio(true);
+
+        Image MapPic = ImageProvider.getImage("images/1_thefirstfloor.png");
+        MapImageView.setImage(MapPic);
+        Group MapGroup = new Group();
+        MapGroup.getChildren().add(MapImageView);
+        Map_ScrollPane.setContent(MapGroup);
+        Map_ScrollPane.setPannable(true);
+        Map_ScrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        Map_ScrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        Map_Slider.minProperty().bind(Map_Split.widthProperty());
+        Map_Slider.setMax(MapPic.getWidth());
+        MapImageView.fitWidthProperty().bind(Map_Slider.valueProperty());
+    }
+
+    private void BuildMapGroup(Image map, Path path){
+        MapImageView.setImage(map);
+        Map_Slider.setMax(map.getWidth());
+
+        if (path.numNodes() < 1) {
+            System.err.println("Can't display map because there is no path.");
+            return;
+        }
+
+        for (MapNode node : path) ShowNodesEdgesHelper.MakeCircle(node.getModelNode());
+        path.edges().stream().map(ShowNodesEdgesHelper::MakeLine);
+        //HideTabs(path);
+    }
+
+    private void ClearMapGroup(){
+        Group group1 = (Group) Map_ScrollPane.getContent();
+        group1.getChildren().remove(1, group1.getChildren().size());
+    }
+
+    private Circle MakeCircle (model.Node N){
+        Circle circle = new Circle();
+        return circle;
+    }
+
+
 }
