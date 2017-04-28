a635b7135668929ca6d015cb859be1aa92c0fa97
Sam Coache <sam.c.coache@gmail.com>
2017-04-27 20:04:28 -0400

    EdgeServiceTest complete

--- src/test/java/service/EdgeServiceTest.java
+++ src/test/java/service/EdgeServiceTest.java
@@ -26,9 +26,9 @@ public class EdgeServiceTest {
     public void setUp() throws Exception {
         EMFProvider.getInstance().useTest();
         // Diagram
-        // NodeB(10,20)---NodeC(20,20)
-        //     |         /
-        //     |        /
+        //    NONE       NodeC(20,20)
+        //               /
+        //              /
         // Node A(10,10)
 
         // Create testNodeA
@@ -38,14 +38,6 @@ public class EdgeServiceTest {
         Coordinate locationA = new Coordinate(x, y, floor);
         String nameA = "testNodeA";
         Node testNodeA = new Node(nameA, locationA);
-
-        // Create testNodeB
-        double xB = 10;
-        double yB = 20;
-        int floorB = 2;
-        Coordinate locationB = new Coordinate(xB, yB, floorB);
-        String nameB = "testNodeB";
-        Node testNodeB = new Node(nameB, locationB);
 
         // Create testNodeC
         double xC = 20;
@@ -57,22 +49,16 @@ public class EdgeServiceTest {
 
         // Add coordinates to coordinate service
         coordinateService.persist(locationA);
-        coordinateService.persist(locationB);
         coordinateService.persist(locationC);
 
         // Add nodes to node service
         nodeService.persist(testNodeA);
-        nodeService.persist(testNodeB);
         nodeService.persist(testNodeC);
 
         // Add edges to nodes
-        Edge AtoB = new Edge(testNodeA, testNodeB, 10);
-        Edge BtoC = new Edge(testNodeB, testNodeC, 10);
         Edge AtoC = new Edge(testNodeA, testNodeC, 14);
 
-        // Add edges to edge service
-        edgeService.persist(AtoB);
-        edgeService.persist(BtoC);
+        // Add edges to edge service;
         edgeService.persist(AtoC);
     }
 
